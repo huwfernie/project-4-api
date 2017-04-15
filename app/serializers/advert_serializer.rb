d62a8980d2320fd79cc445a75646a997e0f17d34
Huw Fernie <HuwFernie@gmail.com>
2017-04-15 08:27:13 +0100

    added active model serializer

---
+++ app/serializers/advert_serializer.rb
@@ -0,0 +1,3 @@
+class AdvertSerializer < ActiveModel::Serializer
+  attributes :id
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :messages
-  resources :offers
-  resources :adverts
-  resources :users
+  scope :api do
+    resources :messages
+    resources :offers
+    resources :adverts
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
