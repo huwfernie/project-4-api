46322f54b2c5734bad1f850b24eca04ffcd7e605
Huw Fernie <HuwFernie@gmail.com>
2017-04-19 09:18:04 +0100

    changed serializer to output created_at for the messages

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,3 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :subject, :body, :sender_id, :reciever_id
+  attributes :id, :subject, :body, :sender_id, :reciever_id, :created_at
 end
